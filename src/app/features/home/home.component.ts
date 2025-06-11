216547f7415f501c4f313d9b3becceb6607d7a01
Danilo Silva <danilo.silva@msn.com>
2025-06-11 15:57:11 -0300

    Add Angular Material and CDK, update styles and routing

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { GridComponent } from '../../shared/components/grid/grid.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
